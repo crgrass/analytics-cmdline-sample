9b4ac978b59424976543d0f954b839e9f38cdfe9
crgrass <crgrass@gmail.com>
2015-10-02 13:28:16 -0400

    Continued work generalizing the four individual centro methods into one method.

--- src/main/java/DataAppCode/ImportCentro.java
+++ src/main/java/DataAppCode/ImportCentro.java
@@ -19,7 +19,11 @@ import com.dropbox.core.DbxException;
 import guiCode.DataAppTest;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.logging.Level;
 
 /**
@@ -66,7 +70,51 @@ public class ImportCentro {
     //Remove any dates in the array that are outside the specified reporting cycle
     CSVReaders.removeInvalidDates(data,  "Centro", DataAppTest.startDate);
     
+    //Now that we have the raw data in ArrayList<String[]> form
+    //We need to group appropriately into a hashmap
+    //We will then iterate through the HashMap and aggregate each entry
     
+    DataAppTest.logger.log(Level.INFO,"Grouping Data by Source, Medium, Campaign and AdContent.");
+    HashMap<GroupID, ArrayList<String[]>> groupedData = importUtils.groupCentroRawData(data, DataAppTest.startDate);
+    
+    DataAppTest.logger.log(Level.INFO,"Aggregating " + medium + " data.");
+    
+    
+    
+  }
+  
+  public void aggregateCentro(HashMap<GroupID,ArrayList<String[]>> rawData,String medium, LocalDate sDate,
+                              LocalDate eDate) {
+    
+    DataAppTest.logger.log(Level.INFO, "Aggregating rows based on Source, Network, Campaing and AdContent");
+    
+    //filter raw data so that only data for one medium remains
+    HashMap<GroupID, ArrayList<String[]>> filteredData = filterMedium(medium, rawData);
+    
+    //Need to generalize DDRecord, Vid Record, Rich Record and Mobile record
+    
+    
+  }
+  
+  
+  
+  
+  public HashMap<GroupID, ArrayList<String[]>> filterMedium(String medium, HashMap<GroupID, ArrayList<String[]>> rawData) {
+    
+  //Iterate through HashMap and place only entries for specified medium into HashMap
+    HashMap<GroupID,ArrayList<String[]>> onlyMedium = new HashMap<GroupID,ArrayList<String[]>>();
+    Iterator<Map.Entry<GroupID, ArrayList<String[]>>> itr = rawData.entrySet().iterator();
+    while (itr.hasNext()) {
+      Map.Entry<GroupID, ArrayList<String[]>> pairs = itr.next();
+      GroupID currID = pairs.getKey();
+      ArrayList<String[]> currArray = pairs.getValue();
+      //This is the sorting that should be removed
+      if (currID.getMedium().equals(medium)) {
+        onlyMedium.put(currID, currArray);
+      }//end of if
+    }//end of while
+    
+    return onlyMedium;
     
   }
 
